b3eb925edb8c8f9aff7754734170c8dd84680f12
smit <smit@topsinfosolutions.com>
2022-05-06 18:07:44 +0530

    logger complete

---
+++ src/config/morgan.js
@@ -0,0 +1,19 @@
+const morgan = require("morgan")
+const logger = require("./winston")
+
+var stream =  {
+    write : (message) => logger.http(message)
+} 
+
+const format = ":method :url :status :res[content-length]  :response-time ms"
+
+const skip = () => {
+    return false
+}
+
+const morgalMiddleware = morgan(format,{
+    stream,
+    skip
+})
+
+module.exports = morgalMiddleware
